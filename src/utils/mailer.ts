5f0ec0733d9943858bbcdef8a75f964451cb49e8
Mohammed Ismail <mahammedismail1996@gmail.com>
2025-09-07 13:21:37 +0300

    feat: update password reset email templates with improved styling and fallback options; refactor mailer to use environment variables for email configuration

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,12 +1,14 @@
 import { Resend } from "resend";
+import { ENV } from "../config/env";
 import { resetEmailHtml, resetEmailText } from "./emailTemplates/resetPasswordTemplates";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(ENV.RESEND_API_KEY);
+const fromEmail = ENV.FROM_EMAIL || ""
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
     try {
         const { data, error } = await resend.emails.send({
-            from: process.env.FROM_EMAIL as string,
+            from: fromEmail,
             to,
             subject,
             html,
@@ -28,16 +30,16 @@ export const sendEmail = async (to: string, subject: string, html: string) => {
 export const sendResetEmail = async (email: string, code: string) => {
   try {
     await resend.emails.send({
-      from: "noreply@yourdomain.com", // must be verified in Resend
+      from: fromEmail, 
       to: email,
       subject: "Password Reset Code",
       html: resetEmailHtml(code, {
-        appName: "InternFinder", // your app name
-        supportEmail: "support@internfinder.com", // your support
-        brandColor: "#16a34a", // optional brand color
-        logoUrl: "https://yourcdn.com/logo.png", // optional logo
+        appName: "Hulu Constructions", // your app name
+        supportEmail: "support@hulconst.com", // your support
+        brandColor: "#104D86", // optional brand color
+        logoUrl: "https://simbatech.et/assets/logo-M8d1WI2n.png", // optional logo
       }),
-      text: resetEmailText(code, "InternFinder"),
+      text: resetEmailText(code, "Hulu Constructions"),
     });
   } catch (error) {
     console.error("Error sending reset email:", error);
